7720a14b526f6af4e804796ad94d33a61c46143f
arung@medallia.com <arung@medallia.com>
2021-11-22 17:37:07 -0800

    Fixed typo

--- index.js
+++ index.js
@@ -6,10 +6,10 @@ function shadowizard(options) {
         options.shadow_type = '0px';
     }
     else {
-        options.shadow_type = '15x';
+        options.shadow_type = '15px';
     }
 
-    images.foreach(image => {
+    images.forEach(image => {
         image.style.boxShadow = `10px 10px ${options.shadow_type} 1px rgba(0,0,0,0.12)`;
         if (options.padding) {
             image.style.padding = '1em'; 
